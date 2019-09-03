2fc8a70d7adfd7ccc5e416ff4b2a170cf96db3ff
shammahk <shammahk1@gmail.com>
2019-09-03 21:07:52 +0100

    added intro page

--- js/intro.js
+++ js/intro.js
@@ -1,1 +1,30 @@
-const $(ele) = document.querySelector(ele);
+const $ = selector => document.querySelector(selector);
+
+
+const changeVisibilty = () => {
+    const intro = $(".intro");
+    const body = $('.body')
+
+
+    intro.classList.remove('show');
+    intro.classList.add('hide');
+
+    body.classList.remove('hide');
+    body.classList.add('show');
+
+    setTimeout(() => {
+        intro.style.display = 'none';
+    }, 1000);
+
+    console.log(intro);
+}
+
+const startApp = () => { 
+    setTimeout(() => {
+        changeVisibilty();
+    }, 3000);
+}
+
+window.addEventListener('load', () => {
+    startApp();
+})
