fb95cfd7b4c6d8aeb1fd09170c60d87967a84dcb
shammahk <shammahk1@gmail.com>
2019-09-05 17:10:33 +0100

    added json file and fill functions

---
+++ js/main.js
@@ -0,0 +1,89 @@
+const fillImages = data => {
+    const imageHolder = [];
+    const imageCont = $('.image-cont');
+    console.log(imageCont);
+
+    for (let i = 0; i < data.imageShow.length; i++) {
+        
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+            <figure class="image-holder" id="image-holder">
+                <img src="${data["imageShow"][i].imgUrl}" alt="studio">
+            </figure>
+            <div class="info">
+                <p class="title">${data["imageShow"][i].title}</p>
+                <p class="time-stamp">${data.imageShow[i].time}</p>
+            </div>
+        `;
+
+        imageCont.appendChild(card);
+        imageHolder.push(card);
+    }
+}
+
+const fillVideos = data => {
+    const videoCont = $('.video-cont');
+    console.log(videoCont);
+
+    for (let i = 0; i < data.videoReel.length; i++) {
+        
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+            <figure class="video-holder" id="video-holder">
+                <img src="${data["videoReel"][i].previewUrl}" alt="studio">
+            </figure>
+            <div class="info">
+                <p class="title">${data["videoReel"][i].title}</p>
+                <p class="time-stamp">${data.videoReel[i].time}</p>
+            </div>
+        `;
+
+        videoCont.appendChild(card);
+    }
+}
+
+const fillMinistered = data => {
+    const logoCont = $('.logo-cont');
+    console.log(logoCont);
+
+    for (let i = 0; i < data.ministered.length; i++) {
+        
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.classList.add('no-shadow');
+        card.innerHTML = `
+            <figure class="church-logo-holder" id="logo-holder">
+                <img src="${data["ministered"][i].imgUrl}" alt="studio">
+            </figure>
+            <div class="info">
+                <p class="title">${data["ministered"][i].title}</p>
+            </div>
+        `;
+
+        logoCont.appendChild(card);
+    }
+
+}
+
+
+
+const fillContent = () => {
+
+    const api = "./js/content.json"
+
+    fetch(api)
+        .then(response => 
+            response.json(api)
+        )
+        .then(data => {
+            console.log(data);
+            fillImages(data);
+            fillVideos(data);
+            fillMinistered(data);
+        })
+    
+}
+
+fillContent();
